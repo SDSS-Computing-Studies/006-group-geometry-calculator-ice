186bca65b83e79ad495d930c6716bd4bd01e0ccf
Sepehr <Sepehrz867@deltalearns.ca>
2021-03-10 09:48:44 -0800

    done

--- assignment.py
+++ assignment.py
@@ -2,44 +2,34 @@
 # Volume Calculator
 # Feel free to rename your variables
 
+import math
 
 def title():
     # Will display a title screen
     # input parameters: none needed
     # output parameters: None
-    # Author: Copper
-    # Modified:
-    # title
-    return None
+    # Author: sepehr
+    # Modified: Sepehr Zolfaghari
+    print("Volume Calculator")
+    print("author: Sepehr Zolfaghari")
+    print("modified by Sepehr Zolfaghari")
+    print("/n")
+    return none
 
 def instructions():
     # Will display instructions
     # input parameters: none needed
     # output parameters: None
-    # Author:
-    # Modified:
-    return None
+    # Author: Sepehr 
+    # Modified: Sepehr Zolfghari
+    print("start")
 
-def getParams(shape):
-    # Will create a list of questions to be asked depending on the shape.
-    # These will be asked so that the user can enter in appropriate values
-    # input parameter: string 
-    # output parameter: return a list containing the prompts for each shape:
-    # example: ["Enter the radius:","Enter the slant height:","Enter the height:"]
-    prompts
-
-    return prompts
-
-def getInputs(questions):
-    # Will prompt the user for inputs for the shape they.
-    # These will be asked so that the user can enter in appropriate values
-    # It will turn all the input data into a list
-    # input parameter: list containing the prompts/questions
-    # output parameter: return a list containing all the measurements of the shape
-    measurements
     
-    return measurements
+    
+    
 
+def square 
+   
 def main():
     # main block of code that will run your program and control program flow
     # You will need to include a while loop to keep repeating the commands until
