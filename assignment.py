9344c5d1acad3e8c061ab7f3ac8cb92823cb24b7
Sepehr <Sepehrz867@deltalearns.ca>
2021-03-12 09:08:01 -0800

    done

--- assignment.py
+++ assignment.py
@@ -46,7 +46,27 @@ def getShape():
     shape=input("enter a shape: ")
     return shape
     
+def getParams(shape):
+    # Will create a list of questions to be asked depending on the shape.
+    # These will be asked so that the user can enter in appropriate values
+    # input parameter: string 
+    # output parameter: return a list containing the prompts for each shape:
+    # example: ["Enter the radius:","Enter the slant height:","Enter the height:"]
+    
+    #r=input("enter the radius: ") # sphere, cylinder, cone
+    #s=input("enter the slant height: ")
+    #h=input("enter the height: ") # cube, retangle, pyramid
+    #w=input("enter the width")
+    #l=input("enter the langth")
+    # triangle
+    #baselength=input("enter the height of the base of a surface of a triangle (calulate the area of the): ")
+    #baselength=input("enter the length of one side of a surface of a triangle: ")
 
+    call forth=["enter the radius: ","enter the height:","enter the width: ","enter the length: ","enter the height of the base of the surface of the triangle: ","enter the length of one side of the surcace of the triangle: "]
+    r,h,w,l,baseheight,baselength,=call forth
+    if shape=="sphere":
+        question=[r]
+        
 
 
 
