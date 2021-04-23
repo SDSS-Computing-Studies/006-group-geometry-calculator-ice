d4b119ea3ad68c9dd3df271a0810b3d00cbc0b9f
Sep-Encryptor <sepehrz867@deltalearns.ca>
2021-04-22 23:21:58 -0700

    done

--- assignment.py
+++ assignment.py
@@ -24,28 +24,13 @@ def instructions():
     # output parameters: None
     # Author: Sepehr 
     # Modified: Sepehr Zolfghari
-    print("start")
-
-    print("instructions: ")
-    print("This program will be asking you to enter a shape that you would want to calculate its volume")
-    print("Then you need to enter that right measuremnets that you will use")
-    print("The volume would be the answer")
-    print("/n")
-    print("Enter yes to remake the program")
-    print("Enter no to close the program")
-    print("/n")
-    print("Shapes accessible: ")
-    print("/n")
-    print("sphere, cylinder, cone, cube, cuboid, pyramid, triangular prism")
-    print("/n")
-    return None
+    
 
 def getShape():
     # input: none needed
     # output: string shape
     # author
-    shape=input("enter a shape: ")
-    return shape
+    
     
 def getParams(shape):
     # Will create a list of questions to be asked depending on the shape.
@@ -63,33 +48,7 @@ def getParams(shape):
     #baselength=input("enter the height of the base of a surface of a triangle (calulate the area of the): ")
     #baselength=input("enter the length of one side of a surface of a triangle: ")
 
-     prompts=["enter the radius: ","enter the height:","enter the width: ","enter the length: ","enter the height of the base of the surface of the triangle: ","enter the length of one side of the surcace of the triangle: "]
     
-    r,h,w,l,baseheight,baselength,=prompts
-    if getShape=="sphere":
-        question=[r]
-        return question
-    elif getShape=="cylinder":
-        question=[r,h]
-        return question
-    elif getShape=="cone":
-        question=[r,h]
-        return question
-    elif getShape=="cube":
-        question=[w]
-        return question
-    elif getShape=="cuboid":
-        question=[h,w,l]
-        return question
-    elif getShape=="pyramid":
-        question=[h,w,l]
-        return question
-    elif getShape=="triangular prism":
-        question=[baseheight,baselength,h]
-        return question
-    else:
-        question=[0]
-        return question
 
 
 def getInputs(question):
@@ -99,70 +58,11 @@ def getInputs(question):
     # input parameter: list containing the prompts/questions
     # output parameter: return a list containing all the measurements of the shape
 
-    a=0
-    List=[]
-    for i in question:
-        if question[a]==0:
-            break
-        else:
-            c=input(question[a])
-            c=float(c)
-            List.append(c)
-            a+=1
-        return List
-
-        def Calculate(measuremnets,shhere):
-
-            if shape=="sphere":
-        v=(4*math.pi*(measurements[0]**3))/3
-        return vars
-    elif shape=="cylinder":
-        v=math.pi*(measurements[0]**2)*measurements[1]
-        return vars
-    elif shape=="cone":
-        v=(math.pi*(measurements[0]**2)*measurements[1])/3
-        return vars
-    elif shape=="cube":
-        v=measurements[0]**3
-        return vars
-    elif shape=="cuboid":
-        v=measurements[0]*measurements[1]*measurements[2]
-        return vars
-    elif shape=="pyramid":
-        v=(measurements[0]*measurements[1]*measurements[2])/3
-        return vars
-    elif shape=="triangular prism"   :
-        v=0.5*measurements[0]*measurements[1]*measurements[2]
-        return vars
-    else:
-        v="not availble"
-        return vars
-
-        title()
-        instructions()
-
+    
 
 def main():
     # main block of code that will run your program and control program flow
     # You will need to include a while loop to keep repeating the commands until
     # the user chooses to exit
     
-    triangle=getShape()
-    # get list of questions
-    questions=getparams(shape)
-    # get list of measurements 
-    measuremnets=getInputs(questions)
-    print("value is "+str(calculate(measuremnets,shape))) # calculate the volume 
-
-
-
- main()
-while True:
-    print("\n")
-    a=input("continue? yes or no: ")
-    print("\n")
-    if a == "yes":
-        main()
-    else:
-        print("end")
-        break
+   
